5bfb0729d5bc4530f463f3f7113530789f33447c
kangadoelcakep-boop <kangadoelcakep@gmail.com>
2025-10-04 23:55:33 +0700

    Update review.js
    API scipt

--- js/review.js
+++ js/review.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const validationMessage = document.getElementById("validation-message");
   const reviewForm = document.getElementById("review-form");
 
-  const API_URL = window.Config?.API_URL || "https://script.google.com/macros/s/AKfycbxxxx/exec";
+  const API_URL = window.Config?.API_URL || "https://script.google.com/macros/s/AKfycbwjJQ69NNajRuYS2_w2mZlK7zY3CHs1pbY2vJvOisRtmMZSwEZJIPcn9u4djtUCe1HqPg/exec";
 
   /* -------------------------
    *  VALIDASI EMAIL
